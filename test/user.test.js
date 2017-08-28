682796be0cfbd0be557d3bcdf170f17b943d306a
madbazsa <madbazsa@gmail.com>
2017-08-28 17:31:44 +0200

    add mailchimp

--- test/user.test.js
+++ test/user.test.js
@@ -2,22 +2,27 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../lib/server';
 import User from '../lib/models/user.model';
+import mailchimp from '../lib/utils/mailchimp';
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe('USER:', () => {
+  const email = 'teszt7@gmail.com';
+
   beforeEach(async () => {
+    await mailchimp.deleteAll();
     await User.remove({});
   });
 
   describe('- Subscribe a user:', () => {
     it('Should get back the new user, if every parameter is valid.', async () => {
-      const newUser = new User({ name: 'Test User', email: 'test@user.com' });
+      const newUser = new User({ name: 'Test User', email });
       const result = await chai.request(server).post('/api/user').send(newUser);
+      console.log(result.body);
       expect(result.body.name).to.equal('Test User');
-      expect(result.body.email).to.equal('test@user.com');
+      expect(result.body.email).to.equal(email);
       expect(await User.count({})).to.equal(1);
     });
 
@@ -29,17 +34,19 @@ describe('USER:', () => {
     });
 
     it('Should get back an error, if email parameter is not valid.', async () => {
-      const email = 'testuser.com';
-      const newUser = new User({ name: 'Test User', email });
+      const invalidEmail = 'testuser.com';
+      const newUser = new User({ name: 'Test User', email: invalidEmail });
       const result = await chai.request(server).post('/api/user').send(newUser);
-      expect(result.body.errors.email.message).to.equal(`${email} is not a valid email address!`);
+      expect(result.body.errors.email.message).to.equal(`${invalidEmail} is not a valid email address!`);
       expect(await User.count({})).to.equal(0);
     });
 
     it('Should get back an error, if email is already exist.', async () => {
-      const newUser = new User({ name: 'Test User', email: 'test@user.com' });
-      await chai.request(server).post('/api/user').send(newUser);
-      await chai.request(server).post('/api/user').send(newUser);
+      const newUser = new User({ name: 'Test User', email });
+      const result1 = await chai.request(server).post('/api/user').send(newUser);
+      expect(result1.body.name).to.equal('Test User');
+      const result2 = await chai.request(server).post('/api/user').send(newUser);
+      expect(result2.body.errors).to.equal(`${newUser.email} is already a list member. Use PUT to insert or update list members.`);
       expect(await User.count({})).to.equal(1);
     });
   });
